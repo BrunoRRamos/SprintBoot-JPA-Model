4a361502b4ef914cbd39224125319b517f16fc01
root <root@DESKTOP-82DTPK2>
2023-07-27 19:32:36 -0300

    DTO medico e inicio Rota POST Medico

---
+++ src/main/java/med/voll/alura/api/medico/DadosCadastradosMedico.java
@@ -0,0 +1,12 @@
+package med.voll.alura.api.medico;
+
+import med.voll.alura.api.endereco.DadosEndereco;
+
+public record DadosCadastradosMedico(
+        String nome,
+        String email,
+        String crm,
+        Especialidade especialidade,
+        DadosEndereco endereco
+    ) {
+}
---
+++ src/main/java/med/voll/alura/api/medico/Especialidade.java
@@ -0,0 +1,9 @@
+package med.voll.alura.api.medico;
+
+public enum Especialidade {
+    ORTOPEDIA,
+    CARDIOLOGIA,
+    GINECOLOGIA,
+    DERMATOLOGIA
+
+}
