433a569a6188dce8389364f56e03c015bfa69c7d
root <root@DESKTOP-82DTPK2>
2023-08-02 09:41:47 -0300

    feat: Listagem de medicos (raw)

--- src/main/java/med/voll/alura/api/controller/MedicoController.java
+++ src/main/java/med/voll/alura/api/controller/MedicoController.java
@@ -1,6 +1,7 @@
 package med.voll.alura.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.alura.api.medico.DadosListagemMedico;
 import med.voll.alura.api.medico.DadosCadastradosMedico;
 import med.voll.alura.api.medico.Medico;
 import med.voll.alura.api.medico.MedicoRepository;
@@ -8,10 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -26,4 +26,10 @@ public class MedicoController {
         repository.save(new Medico(data));
         return new ResponseEntity<>("Médico Cadastrado com Sucesso", HttpStatus.CREATED);
     }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar() {
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    }
+
 }
