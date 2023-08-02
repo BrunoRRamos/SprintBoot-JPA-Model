458d57da62d83d19f4053e9197f905895057cf5c
root <root@DESKTOP-82DTPK2>
2023-08-02 09:51:43 -0300

    feat: Listagem de medicos (Com paginação)

--- src/main/java/med/voll/alura/api/controller/MedicoController.java
+++ src/main/java/med/voll/alura/api/controller/MedicoController.java
@@ -6,6 +6,8 @@ import med.voll.alura.api.medico.DadosCadastradosMedico;
 import med.voll.alura.api.medico.Medico;
 import med.voll.alura.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,9 +29,11 @@ public class MedicoController {
         return new ResponseEntity<>("Médico Cadastrado com Sucesso", HttpStatus.CREATED);
     }
 
+    //Pelo params da requisição pode ser escolhido a quantidade de registros a
+    //serem retornados "medicos?size=(numero de registros)" o default é 20.
     @GetMapping
-    public List<DadosListagemMedico> listar() {
-        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    public Page<DadosListagemMedico> listar(Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 
 }
